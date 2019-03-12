32adc1237e69054456fdff2f96a1b8b1df74609f
Aaron Imming <aaron@binary.com>
2019-03-12 09:35:32 +0800

    Use binaryApi

--- src/botPage/bot/CliTools.js
+++ src/botPage/bot/CliTools.js
@@ -1,13 +1,13 @@
 import Observer from '../../common/utils/observer';
 import TicksService from '../common/TicksService';
-import { generateLiveApiInstance } from '../../common/appId';
+import { binaryApi } from '../../common/appId';
 
 // eslint-disable-next-line import/prefer-default-export
 export const createScope = () => {
     const observer = new Observer();
-    const api = generateLiveApiInstance();
+    const api = binaryApi.api;
 
-    const ticksService = new TicksService(api);
+    const ticksService = new TicksService(binaryApi.api);
 
     return { observer, api, ticksService };
 };
